93401fa4e38cd3e08dd2f4a245a448b583f64761
Craig Spaeth <craigspaeth@Craigs-MacBook-Pro-2.local>
2015-05-16 20:17:38 -0400

    3

--- assets/main.js
+++ assets/main.js
@@ -7,11 +7,21 @@ $(function() {
 
 // Center copy on the page
 var centerCopySections = function() {
-  $('.copy-section, .portfolio-content-container').each(function(i, el) {
-    $(el).css({
-      'margin-top': -Math.round($(el).height() / 2) - SECTION_ADJUST,
-      top: '50%'
-    });
+  $('.copy-section, .centered-content').each(function(i, el) {
+    if($(el).height() > $(window).height()) {
+      $(el).css({
+        'margin-top': 100,
+        'margin-bottom': 100,
+        top: 0,
+        position: 'relative'
+      });
+    } else {
+      $(el).css({
+        'margin-top': -Math.round($(el).height() / 2) - SECTION_ADJUST,
+        top: '50%',
+        position: 'absolute'
+      });      
+    }
   });
 }
 $(window).on('resize', _.debounce(resizeSections));
@@ -19,7 +29,11 @@ $(window).on('resize', _.debounce(resizeSections));
 // Resizes the sections to fill the viewport
 var resizeSections = function () {
   $('.full-screen-section').each(function(i, el) {
-    $(el).css({ 'height': $(window).height() });
+    if($(el).height() > $(window).height()) {
+      $(el).css({ height: 'auto' });
+    } else {
+      $(el).css({ height: $(window).height() });      
+    } 
   });
 }
 $(window).on('resize', _.debounce(resizeSections));
