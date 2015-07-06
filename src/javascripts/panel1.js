e7758b93cd0ce8dfadc861458eb283f31fce49df
Craig Spaeth <craigspaeth@Craigs-MacBook-Pro-2.local>
2015-07-06 01:53:39 -0400

    making progress

--- src/javascripts/panel1.js
+++ src/javascripts/panel1.js
@@ -1,22 +1,3 @@
-$(function() {
-
-  // Toggle hamburger menu
-  $('.main-nav-hamburger').click(function() {
-    $('.main-nav').toggleClass('active');
-  });
-
-  // Toggle class of header
-  $('.panel1').waypoint({
-    handler: function(dir) {
-      if($(window).width() > 768) $('.main-nav').toggleClass('active');
-    },
-    offset: $('.main-nav').outerHeight() * 2
-  });
-
-  // Panel 1 graph
-  setGraph();
-});
-
 
 // Config
 var width = 160;
